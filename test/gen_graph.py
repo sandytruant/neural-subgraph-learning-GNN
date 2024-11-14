b104f26cf7686ef129c5d25da3980aaf9806bfba
sandysun <etonsun2212@gmail.com>
2024-11-14 19:01:16 +0000

    Add spminer demos

--- test/gen_graph.py
+++ test/gen_graph.py
@@ -3,16 +3,22 @@ import pickle
 
 def genQueryGraph():
     G = nx.Graph()
-    G.add_node(1, type="add")
-    G.add_node(2, type="sub")
+    G.add_node(0)
+    G.add_node(1)
+    G.add_node(2)
+    G.add_edge(0, 1)
+    G.add_edge(0, 2)
     G.add_edge(1, 2)
     return G
 
 def genTargetGraph():
     G = nx.Graph()
-    G.add_node(1, type="add")
-    G.add_node(2, type="sub")
-    G.add_node(3, type="add")
+    G.add_node(0)
+    G.add_node(1)
+    G.add_node(2)
+    G.add_node(3)
+    G.add_edge(0, 1)
+    G.add_edge(0, 2)
     G.add_edge(1, 2)
     G.add_edge(1, 3)
     G.add_edge(2, 3)
